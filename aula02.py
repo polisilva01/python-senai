b7b3d13afa6658521f7d225ac722ce6c0da0c0cb
polisilva01 <polianasilvaam123@gmail.com>
2024-12-05 11:26:32 -0300

    aprendi sobre variaveis

---
+++ aula02.py
@@ -0,0 +1,8 @@
+print('qualquer texto')
+
+nome_usuario = "poliana" # tipo - str = string 
+idade_usuario = 17 # tipo - bool = boolean 
+altura_usuario = 1.70 # tipo - float = flutuante
+maior_idade = False # tipo - bool = boolean 
+
+print("seja bem vindo", nome_usuario, sua idade é idade_usuario anos, voce tem altura_usuario metros, voce é maior de idade? maior_idade)
